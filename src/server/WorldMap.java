62e1a2cb6d46a9079e6dee8df10903c5a1add1aa
J <funtimeschaos@rocketmail.com>
2015-04-16 16:57:09 +0300

    Made the worldmap into a singleton and added some half-functions.

--- src/server/WorldMap.java
+++ src/server/WorldMap.java
@@ -10,6 +10,48 @@ import java.util.List;
  *
  */
 public class WorldMap {
-	List<Floor> map;
+	private static WorldMap wm = new WorldMap();
+	private List<Floor> map;
+	private List<Coordinate> loaded;
+	
+	private WorldMap()
+	{
+		//TODO: Do we need a ctor?
+	}
+	
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param player
+	 * @return
+	 */
+	public static Floor get_floor_at(Coordinate c, boolean player)
+	{
+		int index=wm.loaded.indexOf(c);
+		if(index==-1)
+		{
+			if(player)
+			{
+				/* GENERATE NEW MAP */
+				/* return new floor */
+			}
+			else return null;
+		}
+		return (Floor)(wm.map.toArray()[index]);//Correct floor?
+	}
+	
+	/**
+	 *  Simple wrapper for get_floor_at(Coordinate c, boolean player)
+	 * @param x
+	 * @param y
+	 * @param player
+	 * @return
+	 */
+	public static Floor get_floor_at(long x, long y, boolean player)
+	{
+		return get_floor_at(new Coordinate(x,y),player);
+	}
+	
 	
 }
