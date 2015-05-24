9df0e3ec0391cf5928e54cbd5cee1bb4f44395c5
J <funtimeschaos@rocketmail.com>
2015-05-24 22:49:17 +0300

    Added initial mail sending function

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,7 +4,17 @@ package server;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -14,6 +24,10 @@ public class Server extends Thread {
 
 	Socket s;
 	int num;
+	private static String s_usr; //Sender user for email sending
+	private static String s_pwd; //Sender password for email sending
+	private static String s_host;//Sender host for email sending
+	private static final String data_file="email.txt";
 	//private ServerSocket servers;
 	//private BufferedReader input;
 	//private PrintStream output;
@@ -23,7 +37,7 @@ public class Server extends Thread {
 	
 
 	public static void main(String[] args) {
-
+		get_sender_email();
 		try {
 			int i = 0; // counter for connected clients.
 
@@ -91,7 +105,91 @@ public class Server extends Thread {
 		
 	}
 	
+	public static void get_sender_email()
+	{
+		String line;
+		try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(data_file);
 
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            line = bufferedReader.readLine();
+            s_host = new String(line);
+            
+            line = bufferedReader.readLine();
+            s_usr = new String(line);
+
+            line = bufferedReader.readLine();
+            s_pwd = new String(line);
+//            System.out.println(line);
+
+            // Always close files.
+            bufferedReader.close();            
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                data_file + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + data_file + "'");                   
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+	}
+	
+	/**
+	 * This function registers a new user and sends them a verification email
+	 * @param username
+	 * @param email
+	 * @param password
+	 * @param Salt
+	 * @param Auth_Code
+	 */
+	public static void Register(String username, String email, String password, String Salt, String Auth_Code)
+	{
+		Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", s_host);
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(s_usr, s_pwd);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("noreply@mocs.il"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(email));
+            message.setSubject("Thank you for registering!");
+            message.setText("Greeting " + username
+                + ",\n\n Your authentication code is:"
+                + Auth_Code
+                + "Please copy and paste this into the client to complete your registration!");
+
+            Transport.send(message);
+
+            System.out.println("Sent mail to " + email + "...\n");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        
+        //SQL.add_new_user(blahblah)//This function not implemented yet
+	}
+	
 	/*	*//**
 	 * @param args
 	 */
