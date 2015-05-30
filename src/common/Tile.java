e12fc4497b7869a5b31e4f39b9e5642dea224605
J <funtimeschaos@rocketmail.com>
2015-05-30 18:00:33 +0300

    Added Tile copy ctor

--- src/common/Tile.java
+++ src/common/Tile.java
@@ -36,6 +36,18 @@ public class Tile implements Serializable {
 		c = new Coordinate(x,y);
 	}
 	
+	public Tile(Coordinate co, FloorType ft, MapObjectType mot){
+		f = ft;
+		mo = mot;
+		c = new Coordinate(co);
+	}
+	
+	public Tile(Tile other){
+		f=other.f;
+		mo=other.mo;
+		c = new Coordinate(other.c);
+	}
+	
 	public void setFloorType(FloorType ft)
 	{
 		f=ft;
@@ -69,19 +81,19 @@ public class Tile implements Serializable {
 		return true;
 	}
 	
-	public boolean canMoveOn(){
-		return (f != null && f.canMoveOn()) && (mo == null || mo.canMoveOn());
-	}
-	
-	public boolean canPickUp(){
-		return (mo != null && mo.canPickUp());
-	}
-	
-	public boolean canHarvest(){ 
-		return (mo != null && mo.canHarvest());
-	}
-	
-	public boolean canAttack(){
-		return (mo != null && mo.canAttack());
-	}
+//	public boolean canMoveOn(){
+//		return (f != null && f.canMoveOn()) && (mo == null || mo.canMoveOn());
+//	}
+//	
+//	public boolean canPickUp(){
+//		return (mo != null && mo.canPickUp());
+//	}
+//	
+//	public boolean canHarvest(){ 
+//		return (mo != null && mo.canHarvest());
+//	}
+//	
+//	public boolean canAttack(){
+//		return (mo != null && mo.canAttack());
+//	}
 }
