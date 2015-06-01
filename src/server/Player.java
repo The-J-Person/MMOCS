1a4a9f6b11331032e84b7af4132a167924319484
J <funtimeschaos@rocketmail.com>
2015-06-01 04:48:46 +0300

    Fixed compilation error

--- src/server/Player.java
+++ src/server/Player.java
@@ -34,7 +34,7 @@ public class Player implements MapObject {
 		C=DataBase.GetPlayerCoordinate(id);
 		Inventory = new Hashtable<Resource,Integer>();
 		updateLog = new Stack<Update>();
-		WorldMap.getInstance().
+		WorldMap.getInstance();
 		//TODO add inventory
 	}
 	
